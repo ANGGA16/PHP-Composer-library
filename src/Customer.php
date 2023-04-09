cb664819e5d8fe8bbf7949d2ba7ed5fc2a471884
ANGGA16 <dosqangga45@gmail.com>
2023-04-09 13:15:01 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bikinbalik\BelajarPhpComposerLibrary;
+
+class Customer {
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string 
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
